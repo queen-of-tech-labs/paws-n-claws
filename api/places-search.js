5a6fcb4258154bf7be328f622d65241f02d6bec8
Kitty Queen <queen@royaltytechlabs.com>
2026-03-15 19:56:49 -0400

    revert 1

--- api/places-search.js
+++ api/places-search.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
       // Nearby search with optional text query
       const url = 'https://places.googleapis.com/v1/places:searchNearby';
       const body = {
-        includedTypes: ['veterinary_care', 'pet_store'],
+        includedTypes: ['veterinary_care'],
         maxResultCount: 20,
         locationRestriction: {
           circle: {
