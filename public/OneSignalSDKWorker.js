921cc71e682453262aa505595382d6a0cf559038
Kitty Queen <queen@royaltytechlabs.com>
2026-03-16 12:52:12 -0400

    service worker update

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -2,3 +2,10 @@
 // OneSignal requires this file to be at the root of your site
 
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+// Required by OneSignal v16 - must be on initial evaluation
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
